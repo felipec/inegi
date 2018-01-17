19353dcd47607e0f893751b4b86566101ece9452
Felipe Contreras <felipe.contreras@gmail.com>
2018-01-17 16:01:23 -0600

    lib: add documentation to the code
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

--- lib/dist.rb
+++ lib/dist.rb
@@ -1,3 +1,8 @@
+# Generates a list of probabilities.
+#
+# To generate quartiles (1/4, 2/4, 3/4):
+#     gen_probs(4)
+#
 def gen_probs(num, from=0, to=1)
   step = (to - from) / num.to_f
   (1..(num - 1)).map { |e| from + e * step }
@@ -7,6 +12,11 @@ DECILES = gen_probs(10)
 TERCILES = gen_probs(3)
 QUARTILES = gen_probs(4)
 
+# Calculates weighted quantiles.
+#
+# The +data+ is a list of pairs of values and their corresponding weight:
+#     [10, 4], [20, 3], [30, 2]
+#
 def wquantiles(data, probs=QUARTILES)
   grouped = data.group_by(&:first).map { |a, b| [a, b.reduce(0) { |sum, e| sum + e.last }] }
   values, weights = grouped.sort_by(&:first).transpose
@@ -27,6 +37,11 @@ def wquantiles(data, probs=QUARTILES)
   end
 end
 
+# Calculates the Gini coefficient.
+#
+# The +data+ is a list of pairs of values and their corresponding weight:
+#     [10, 4], [20, 3], [30, 2]
+#
 def gini(data)
   sorted = data.sort_by(&:first)
   sum_values = 0
@@ -44,6 +59,13 @@ def gini(data)
   return 1 - 2 * (total / (sum_values.to_f * sum_weights.to_f))
 end
 
+# Calculates averages per group.
+#
+# The +data+ is a list of pairs of values and their corresponding weight:
+#     [10, 4], [20, 3], [30, 2]
+#
+# This would generate _five_ averages for _four_ quantiles.
+#
 def average_quantiles(data, probs=QUARTILES)
   data = data.sort_by(&:first)
 
