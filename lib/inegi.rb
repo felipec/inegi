33a0a7aef935c3ec570a5206e44f91356bdee59f
Felipe Contreras <felipe.contreras@gmail.com>
2018-01-17 01:21:03 -0600

    Add work income quantiles
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

--- lib/inegi.rb
+++ lib/inegi.rb
@@ -1,5 +1,6 @@
 $ingresos = []
 $ingresos_pc = []
+$ingresos_trab_pc = []
 
 open('ncv_concentrado_2014_concil_2010.csv').each_with_index do |l,i|
   next if i == 0
@@ -9,9 +10,15 @@ open('ncv_concentrado_2014_concil_2010.csv').each_with_index do |l,i|
   integ = cols[13].to_i
   ingreso = cols[23].to_f
 
+  ocupados = cols[20].to_i
+  ingtrab = cols[25].to_i
+
   # por hogar
   $ingresos << [ingreso, factor]
 
   # per capita
   $ingresos_pc << [ingreso / integ, factor * integ]
+
+  # trabajo per capita
+  $ingresos_trab_pc << [ingtrab / ocupados, factor * ocupados] unless ingtrab == 0 or ocupados == 0
 end
