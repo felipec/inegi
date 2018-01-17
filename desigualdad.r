0edc9ba6be66434cce1d826942f561ed122c0cdc
Felipe Contreras <felipe.contreras@gmail.com>
2018-01-17 01:23:10 -0600

    Add inequality R script
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

---
+++ desigualdad.r
@@ -0,0 +1,34 @@
+# install.packages('ineq')
+
+data = read.csv("ncv_concentrado_2014_concil_2010.csv")
+
+values = data$ing_cor
+weights = data$factor_hog
+integ = data$tot_integ
+
+# Per cápita
+values = data$ing_cor / data$tot_integ
+weights = data$factor_hog * data$tot_integ
+
+library(ineq)
+l <- Lc(values / 3, weights)
+
+png('lorenz.png')
+plot(l, general=TRUE, col="red")
+dev.off()
+
+ineq_curve <- function(x, n)
+{
+    o <- order(values)
+    x <- x[o]
+    n <- n[o]
+
+    p <- (cumsum(n) / sum(n)) * 100
+
+    plot(p, x,
+         type="l", col="blue", xlab="poblacion", ylab="ingreso")
+}
+
+png('desigualdad.png')
+ineq_curve(values / 3, weights)
+dev.off()
